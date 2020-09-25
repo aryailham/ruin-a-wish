f9ad147eaf64a3065a5cd38ccc6e889437de957b
Arya <aryailham05@users.github.com>
2020-09-25 21:50:12 +0700

    new posts already working

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Aux from "../../../hoc/Auxiliary";
+import classes from "./Input.module.css";
 
 const input = (props) => (
   <Aux>
     <input
+      className={classes.InputField}
       type="text"
       placeholder="insert your wish..."
       onChange={props.changed}
     ></input>
-    <button onClick={props.clicked}>submit</button>
+    <button onClick={props.clicked} className={classes.Button}>
+      submit
+    </button>
   </Aux>
 );
 
