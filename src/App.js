f8bb9d15e10ab9ecb6d044ad3fcfe7f8651e0369
Arya <aryailham05@users.github.com>
2020-09-26 10:05:50 +0700

    newPost done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import NewPost from "./containers/NewPost/NewPost";
+import AllPost from "./containers/AllPost/AllPost";
 const App = () => {
   return (
     <Layout>
       <Switch>
-        <Route path="/" render={() => <h1>All Post</h1>} exact />
+        <Route path="/" render={() => <AllPost />} exact />
         <Route path="/new-post" component={NewPost} exact />
       </Switch>
     </Layout>
